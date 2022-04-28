851a2abdee27b4e2dbaa706dc1ff45196423fcf5
Aadhar gaur <adhar.gaur@matellio.com>
2022-04-28 15:19:45 +0530

    First Commit

---
+++ src/TimezonesController.php
@@ -0,0 +1,12 @@
+namespace PCB\TimeZones;
+
+use Carbon\Carbon;
+use App\Http\Controllers\Controller;
+
+class TimezonesController extends Controller
+{
+    public function index($timezone)
+    {
+        echo Carbon::now($timezone)->toDateTimeString();
+    }
+}
